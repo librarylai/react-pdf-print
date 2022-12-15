719afd921b7aface3ea4ee9cd46921b03dd2bb82
librarylai <library0515@gmail.com>
2022-12-15 14:56:13 +0800

    feat: fix helmet

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,17 @@ import { ThemeProvider, createTheme } from '@mui/material/styles'
 import theme from '@/theme/theme'
 import { Provider } from 'react-redux'
 import store from '@/redux/store'
+import { HelmetProvider } from 'react-helmet-async'
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <Navbar />
-          <Component {...pageProps} />
-        </ThemeProvider>
+        <HelmetProvider>
+          <ThemeProvider theme={theme}>
+            <Navbar />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </HelmetProvider>
       </Provider>
     </>
   )
